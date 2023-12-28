20beed8b34c2747aa0de94bc5ed59d87096a615f
Masterchinedum <chinedu02k@gmail.com>
2023-12-28 06:06:08 +0100

    Refactor Tcard component and update Travel CSS

--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import Tnavbar from './Tnavbar';
 import Tcard from './Tcard';
+import Traveldata from './Traveldata';
 import './Travel.css'
 
 
 function Travel() {
+
+  let Traveljournal = Traveldata.map((Travel) => {
+    return (
+      <Tcard 
+      key = {Travel.id}
+      {...Travel}
+      />
+    )
+  })
   return (
     <div className='Travel'>
         <Tnavbar />
-        <Tcard />
+        {Traveljournal}
     </div>
   )
 }
---
+++ src/components/Traveldata.js
@@ -0,0 +1,29 @@
+const Traveldata = [
+    {
+        id: 1,
+        img: 'https://source.unsplash.com/WLxQvbMyfas',
+        title: 'Mount Fuji',
+        date: '12 Jan, 2021 - 24 Jan, 2021',
+        location: 'JAPAN',
+        description: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). It is a popular tourist site for both Japanese and foreign visitors.',
+    }, 
+    {
+        id: 2,
+        img: 'https://source.unsplash.com/JmuyB_LibRo',
+        title: 'Sydney Opera House',
+        Date: '27 May, 2021 - 8 Jun, 2021',
+        location: 'Australia',
+        description: `The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.`,
+    }, 
+    {
+        id: 3,
+        img: 'https://source.unsplash.com/3PeSjpLVtLg',
+        title: 'Geirangerfjord',
+        Date: '01 Oct, 2021 - 18 Nov, 2021',
+        location: 'Norway',
+        description: 'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.',
+    }, 
+    
+]
+
+export default Traveldata
